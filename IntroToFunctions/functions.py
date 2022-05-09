e7ec662a3407b2ad84bca62c75e0b0ebce22ac32
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-05-09 22:58:03 +0200

    Updated IntroToFunctions

--- IntroToFunctions/functions.py
+++ IntroToFunctions/functions.py
@@ -19,3 +19,21 @@ def isBiggerThan5(n):
 
 isBiggerThan5(3)
 isBiggerThan5(42)
+
+# Function with return value
+def multiplyByTwo(n):
+    return n * 2
+
+varToMultiply = 7
+print(varToMultiply,"* 2 =", multiplyByTwo(varToMultiply))
+
+# Function that returns a tuple
+def getTuple():
+    stringValue = "This is a string"
+    numberValue = 42
+    return stringValue, numberValue
+
+value1,value2 = getTuple()
+print(getTuple())
+print(value1)
+print(value2)
