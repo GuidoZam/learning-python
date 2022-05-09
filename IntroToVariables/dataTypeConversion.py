6ff9e6c95f4c07984114cb0f6c7b1fd797197a75
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-05-09 23:36:51 +0200

    Added IntroToVariables

---
+++ IntroToVariables/dataTypeConversion.py
@@ -0,0 +1,67 @@
+# Converts x to an integer. base specifies the base if x is a string	
+# int(x [,base])
+print(int("2"))
+
+# Converts x to a long integer. base specifies the base if x is a string
+# long(x [,base] )
+
+
+# Converts x to a floating-point number
+# float(x)
+
+
+# Creates a complex number
+# complex(real [,imag])
+
+
+# Converts object x to a string representation
+# str(x)
+
+
+# Converts object x to an expression string.
+# repr(x)
+
+
+
+# Evaluates a string and returns an object
+# eval(str)
+
+
+# Converts s to a tuple
+# tuple(s)
+
+
+# Converts s to a list
+# list(s)
+
+
+# Converts s to a set
+# set(s)
+
+
+# Creates a dictionary. d must be a sequence of (key,value) tuples
+# dict(d)
+
+
+# Converts s to a frozen set
+# frozenset(s)
+
+
+# Converts an integer to a character
+# chr(x)
+
+
+# Converts an integer to a Unicode character
+# unichr(x)
+
+
+# Converts a single character to its integer value
+# ord(x)
+
+
+# Converts an integer to a hexadecimal string
+# hex(x)
+
+
+# Converts an integer to an octal string
+# oct(x)
