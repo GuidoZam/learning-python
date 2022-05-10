b3803cb717daa3b24967bb3b08f83791005cd77a
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-05-10 21:53:08 +0200

    Updated IntroToVariables

--- IntroToVariables/dataTypeConversion.py
+++ IntroToVariables/dataTypeConversion.py
@@ -32,43 +32,58 @@ describe(r, repr(r))
 
 # Evaluates a string and returns an object
 # eval(str)
-
+stringObject = "{'test':'this is a test property'}"
+o = eval(stringObject)
+describe(stringObject, o)
+print("The property value is:", o["test"])
 
 # Converts s to a tuple
 # tuple(s)
-
+tupleString = "tuple string"
+t = tuple(tupleString)
+describe(tupleString, t)
 
 # Converts s to a list
 # list(s)
-
+listString = "Python"
+l = list(listString)
+describe(listString, l)
 
 # Converts s to a set
 # set(s)
-
+sourceList = [ 3, 4, 1, 4, 5 ]
+set = set(sourceList)
+describe(sourceList, set)
 
 # Creates a dictionary. d must be a sequence of (key,value) tuples
 # dict(d)
+dictVal = {"yy":"John","zz":"Doe"}
+describe(dictVal, dict(dictVal))
 
-
-# Converts s to a frozen set
+# Converts s to a frozen set, frozen sets are immutable
 # frozenset(s)
-
+frozenSet = frozenset(set)
+describe(set, frozenSet)
 
 # Converts an integer to a character
 # chr(x)
+i = 76
+describe(i, chr(i))
 
-
-# Converts an integer to a Unicode character
-# unichr(x)
-
+for x in range(33,40):
+    print(x," - ", chr(x))
 
 # Converts a single character to its integer value
 # ord(x)
-
+for x in range(41,49):
+    print(chr(x)," - ", ord(chr(x)))
 
 # Converts an integer to a hexadecimal string
 # hex(x)
-
+for x in range(49,55):
+    print(hex(x)," - ", chr(x))
 
 # Converts an integer to an octal string
 # oct(x)
+for x in range(56,63):
+    print(chr(x)," - ", oct(x))
